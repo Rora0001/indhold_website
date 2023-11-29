b2524d33cda8877aa403c53fdb3cc485666a2f6e
Rose-Marie <rora0001@stud.kea.dk>
2023-11-29 08:52:44 +0100

    færdigt projekt

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var heading = document.querySelector("h1");
+  // Ingen behov for at tilføje "show" klassen eller ændre gennemsigtighed via JavaScript
+});
